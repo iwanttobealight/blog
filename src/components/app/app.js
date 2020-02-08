c75eb10ba10bcb0ba8cadafab5cf41e5ec51f3d2
iwanttobealight <iwanttobealight@yandex.ru>
2020-02-08 23:16:48 +0300

    update

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 export const App = () => {
   return <div className="app">
     <h1>Daria Moreno-Gogoleva</h1>
-    <h2 style={{ textAlign: 'right' }}>blog</h2>
+    <h2 style={{ textAlign: 'right' }}>my blog</h2>
   </div>
 }
 
