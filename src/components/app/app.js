ab5370bcfe9c711ef2882d7ae224efec1dad6196
iwanttobealight <iwanttobealight@yandex.ru>
2020-02-16 23:51:50 +0300

    update button

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,9 @@
 import * as React from 'react';
-import { Button } from './button';
+
 
 export const App = () => {
   return <div className="app">
     <h1>Daria Moreno-Gogoleva</h1>
-    <Button />
     <h2 style={{ textAlign: 'right' }}>my blog</h2>
   </div>
 }
--- src/components/app/button.jsx
+++ src/components/app/button.jsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 
-
-export const Button = () => {
-  return (<button color="blue" shadowSize={2}>click me</button>)
+export const Button = (props) => {
+  const { color, shadowSize } = props
+  return (
+    <button style={{ color: `${color}`, shadowSize: `${shadowSize}` }}>
+      click me
+    </button>
+  )
 }
