69357128829c5888652184680ba5111224f01ce5
iwanttobealight <iwanttobealight@yandex.ru>
2020-04-23 23:23:16 +0300

    add photo

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css'
+
+import fs from 'fs'
 
 export const App = () => {
+
+  const buffer = fs.readFileSync(__dirname + '/../../../assets/images/me.png')
+
+
   return (<main>
     <h1>Дарья Морено-Гоголева, веб-разработчица</h1>
     <section className="about">
+      <img className="image" src={`data:image/png;base64,${buffer.toString('base64')}`} alt="me"/>
       <div className="text-wrapper">
         <p>живу в Петербурге, делаю фронтенд в <a target="_blank" rel="noopener noreferrer" href="https://breadhead.ru">хлебушке</a></p>
         <p>люблю тесты и чистый код</p>
