59df1f3b361c71389b5381a8d7e4ed6420a623ba
iwanttobealight <iwanttobealight@yandex.ru>
2020-04-23 10:30:08 +0300

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-
-class HelloMessage extends React.Component {
-  render() {
-      return <div>
-          <div className="container">
-              <h1>Hello {this.props.name}</h1>
-          </div>
-      </div>
-  }
+const HelloMessage = ({ name }) => {
+    return <div>
+        <div className="container">
+            <h1>Hello {name}</h1>
+        </div>
+    </div>
 }
 
 let App = document.getElementById("root");
